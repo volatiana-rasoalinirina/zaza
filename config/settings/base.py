88c3c930bdddc2daa1e950b1faeb4544dbd15471
Volatiana Rasoalinirina <volatiana.rasoalinirina@devngo.fr>
2026-05-02 16:19:19 +0300

    create TenantMiddleware

--- config/settings/base.py
+++ config/settings/base.py
@@ -24,6 +24,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'apps.accounts.middleware.TenantMiddleware',
 ]
 
 ROOT_URLCONF = 'config.urls'
